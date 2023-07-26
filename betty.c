bf6515b6d3a8fffa62d3415412bcd9421807b044
Joshuba Bubune <joshua.legithive@gmail.com>
2023-07-26 09:37:02 -0700

    Simple_shell

--- betty.c
+++ betty.c
@@ -1,10 +1,15 @@
 /**
  * sum - Function to sum int
- * Return: Void
+ * Return: Result of summation
  */
-void sum(void)
+int sum(void)
 {
 	int a, b, sum;
 
+	a = 9;
+	b = 10;
+
 	sum = a + b;
+
+	return (sum);
 }
