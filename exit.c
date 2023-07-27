748c70de3c94154d8442ae85edd3117e1f0b09fd
Joshua Bubune <joshua.legithive@gmail.com>
2023-07-27 10:48:22 -0700

    Simple_shell

--- exit.c
+++ exit.c
@@ -12,10 +12,6 @@ void ex_it(char *buf, char **argv, char **args, int a, int i)
 	char *status_str, *err, str[INIT];
 	int exit_status;
 
-	(void)err;
-	(void)args;
-	(void)a;
-
 	err = "exit: Illegal number";
 	if (str_len(buf) > 4)
 	{
@@ -33,6 +29,7 @@ void ex_it(char *buf, char **argv, char **args, int a, int i)
 	{
 		free_arr(argv, i);
 		free(buf);
+		pr_f(STDERR_FILENO, "%s: %d: %s: %s\n", args[0], a, err, str);
 		exit(2);
 	}
 	}
